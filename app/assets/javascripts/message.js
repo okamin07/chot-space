645c6f6c27e52d382fcfee6b96ebf7b6747e1566
okamin07 <ryoya.okamura@di-v.co.jp>
2020-03-30 17:05:27 +0900

    fix

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    var presenceImage = message.image? `<img src=${message.image} ></img>`:``
+    var html = 
+    `<div class="message">
+      <div class="message__top">
+        <div class="user">
+        ${message.user_name}
+        </div>
+        <div class="time">
+        ${message.created_at}
+        </div>
+      </div>
+      <div class="message__bottom">
+        <dix class="text">
+          <p class="lower-message__content">
+          ${message.content}
+          </p>
+        </div>
+      </div>
+        ${presenceImage}
+    </div>`
+    return html;
+  }
+  $("#new_message").on('submit',function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $("form").attr('action');
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data:formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".messages").append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $(".new_message")[0].reset();
+      $(".submit").removeAttr("disabled");
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  })
+})
